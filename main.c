9affa7cbfa3188dc4c8dea3f732608e6a778c870
Ismail Alashqar <ialashqa@lab3r6s13.42abudhabi.ae>
2024-04-30 17:05:18 +0400

    push

--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: ialashqa <ialashqa@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/26 20:01:17 by ialashqa          #+#    #+#             */
-/*   Updated: 2024/04/30 15:13:33 by ialashqa         ###   ########.fr       */
+/*   Updated: 2024/04/30 17:01:36 by ialashqa         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,11 +14,13 @@
 
 int	press_key(int key, void *param)
 {
+	t_data	data;
+
 	(void) param;
 	ft_printf("Key pressed: %d\n", key);
 	if (key == 53)
 	{
-		mlx_destroy_window(data->mlx, data->mlx_window);
+		mlx_destroy_window(data.mlx, data.mlx_window);
 		exit(0);
 	}
 	return (0);
@@ -31,15 +33,23 @@ int	mouse(int x, int y, void *param)
 	return (0);
 }
 
+int	close_window(void *param)
+{
+	t_data	data;
+
+	(void) param;
+	mlx_destroy_window(data.mlx, data.mlx_window);
+	exit(0);
+}
+
 int	main(void)
 {
-	void	*mlx;
-	void	*mlx_window;
+	t_data	data;
 
-	mlx = mlx_init();
-	mlx_window = mlx_new_window(mlx, 1080, 1000, "Test window");
-	mlx_pixel_put(mlx, mlx_window, 250, 250, 0xFFBBBB);
-	mlx_hook(mlx_window, 2, 1L << 0, press_key, NULL);
-	mlx_hook(mlx_window, 6, 1L << 6, mouse, NULL);
-	mlx_loop(mlx);
+	data.mlx = mlx_init();
+	data.mlx_window = mlx_new_window(data.mlx, 1080, 1000, "Test window");
+	mlx_key_hook(data.mlx_window, press_key, NULL);
+	mlx_hook(data.mlx_window, 6, 1L << 6, mouse, NULL);
+	mlx_hook(data.mlx_window, 17, 1L << 17, close_window, NULL);
+	mlx_loop(data.mlx);
 }
